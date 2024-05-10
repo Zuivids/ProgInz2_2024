e9ba0ad55b46c69dc3f23b25ec195dbe3d8f91e8
Zuivids <martins.zaimis@gmail.com>
2024-05-10 11:56:45 +0300

    Added Student class with lombok,validation and jpa

---
+++ src/main/java/com/example/model/Student.java
@@ -0,0 +1,47 @@
+package com.example.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table(name="StudentTable")
+@Entity
+public class Student {
+	@Id
+	@Column(name="Ids")
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private int ids;
+	
+	@Column(name="Name")
+	@NotNull
+	@Size(min = 3, max = 50)
+	//	TODO regex
+	private String name;
+	
+	@Column(name="Surname")
+	@NotNull
+	@Size(min = 3, max = 50)
+	//	TODO regex
+	private String surname;
+	
+	public Student(String name, String surname) {
+		setName(name);
+		setSurname(surname);
+	}
+	
+}
