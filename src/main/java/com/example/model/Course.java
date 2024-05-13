d54eb3a578bd696f3b0cf0917c47ff9020581789
Zuivids <martins.zaimis@gmail.com>
2024-05-13 09:27:01 +0300

    Updated: added OneToOne linkage between Professor and Course

--- src/main/java/com/example/model/Course.java
+++ src/main/java/com/example/model/Course.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -20,25 +22,34 @@ import lombok.ToString;
 @Setter
 @NoArgsConstructor
 @ToString
-@Table(name="CourseTable")
+@Table(name = "CourseTable")
 @Entity
 public class Course {
 	@Id
-	@Column(name="Id_c")
+	@Column(name = "Id_c")
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Setter(value = AccessLevel.NONE)
 	private int id_c;
-	
-	@Column(name="Title")
+
+	@Column(name = "Title")
 	@NotNull
 	@Size(min = 4, max = 50)
 	private String title;
-	
-	@Column(name="Credit Points")
+
+	@Column(name = "CreditPoints")
 	@Min(1)
 	@Max(20)
 	private int creditPoints;
-	
-	//TODO uztaisit konstruktoru pec saites pievienoshanas ar profesoru
-	
+
+	// TODO uztaisit konstruktoru pec saites pievienoshanas ar profesoru
+
+	@OneToOne
+	@JoinColumn(name = "Id_p")
+	private Professor professor;
+
+	public Course(String title, int creditpoints, Professor profressor) {
+		setTitle(title);
+		setCreditPoints(creditpoints);
+		setProfessor(professor);
+	}
 }
--- src/main/java/com/example/model/Professor.java
+++ src/main/java/com/example/model/Professor.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -18,35 +19,39 @@ import lombok.ToString;
 @Setter
 @NoArgsConstructor
 @ToString
-@Table(name="ProfessorTable")
+@Table(name = "ProfessorTable")
 @Entity
 public class Professor {
 	@Id
-	@Column(name="Id_p")
-	@GeneratedValue(strategy= GenerationType.AUTO)
+	@Column(name = "Id_p")
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Setter(value = AccessLevel.NONE)
-	private int id_s;
-	
-	@Column(name="Name")
+	private int id_p;
+
+	@Column(name = "Name")
 	@NotNull
 	@Size(min = 3, max = 50)
-	//	TODO regex
+	// TODO regex
 	private String name;
-	
-	@Column(name="Surname")
+
+	@Column(name = "Surname")
 	@NotNull
 	@Size(min = 3, max = 50)
-	//	TODO regex
+	// TODO regex
 	private String surname;
-	
-	@Column(name="Degree")
+
+	@Column(name = "Degree")
 	@NotNull
 	private Degree degree;
-	
+
 	public Professor(String name, String surname, Degree degree) {
 		setName(name);
 		setSurname(surname);
 		setDegree(degree);
 	}
-	
+
+	@OneToOne(mappedBy = "professor")
+	@ToString.Exclude
+	private Course course;
+
 }
